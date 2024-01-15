037326fefb0b6e4d7e5099722846bbf78bb0d09e
Yuanxp1997 <127631889+Yuanxp1997@users.noreply.github.com>
2024-01-15 12:22:54 -0700

    version 1.0

--- threejs/client/src/App.jsx
+++ threejs/client/src/App.jsx
@@ -1,19 +1,15 @@
-import Home from './pages/Home'
-import Customizer from './pages/Customizer'
-import Canvas from './canvas'
-
-
+import Home from "./pages/Home";
+import Customizer from "./pages/Customizer";
+import Canvas from "./canvas";
 
 function App() {
- 
   return (
-    <main className='app transition-all ease-in supports-[height:100cqh]:h-[100cqh] supports-[height:100svh]:h-[100svh]'>
+    <main className="app transition-all ease-in supports-[height:100cqh]:h-[100cqh] supports-[height:100svh]:h-[100svh]">
       <Home />
       <Canvas />
       <Customizer />
-      
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
